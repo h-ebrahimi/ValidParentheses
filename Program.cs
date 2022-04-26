adf64ced5d7cc7751b005d93b515e119e4cba2de
Lenovo <Lenovo@DESKTOP-067GH63>
2022-04-26 12:13:34 +0430

    add solutions

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ValidParantheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "([)]";
+            Solution solution = new Solution();
+            var res =solution.IsValid(s);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidParantheses
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            char[] chLeft = { '{', '[', '(' };
+            var sc = new Stack<char>();
+            foreach (var c in s)
+            {
+
+                if (chLeft.Contains(c))
+                {
+                    sc.Push(c);
+                    continue;
+                }
+
+                if (sc.Count == 0) return false;
+
+                switch (sc.Pop())
+                {
+                    case '[' when c != ']':
+                    case '(' when c != ')':
+                    case '{' when c != '}':
+                        return false;
+                }
+            }
+
+            return sc.Count == 0;
+        }
+    }
+}
